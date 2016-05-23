2a76cd08fdd04a4fba8d31b9582b0ca4748f417c
Tim Barton <tim@vouchercloud.com>
2016-05-23 17:56:52 +0100

    Updated the pre-build script

--- CircuitBreaker/pre-build.bat
+++ CircuitBreaker/pre-build.bat
@@ -1,2 +1,31 @@
-call %GitVersion% /output buildserver
-call %GitVersion% /updateassemblyinfo true
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+
+set version=
+if not "%PackageVersion%" == "" (
+   set version=-Version %GitVersion.ClassicVersionWithTag%
+)
+REM Package restore
+call %NuGet% restore CircuitBreaker.Tests\packages.config -OutputDirectory %cd%\packages -NonInteractive
+
+REM Build
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild IDL.Net.CircuitBreaker.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+if not "%errorlevel%"=="0" goto failure
+
+REM Unit tests
+"%GallioEcho%" CircuitBreaker.Tests\bin\%config%\CircuitBreaker.Tests.dll
+if not "%errorlevel%"=="0" goto failure
+
+REM Package
+mkdir Build
+call %nuget% pack "CircuitBreaker\CircuitBreaker.nuspec" -symbols -o Build -p Configuration=%config% %version%
+if not "%errorlevel%"=="0" goto failure
+
+:success
+exit 0
+
+:failure
+exit -1
