291429b103c7cf1a27d31edbc68a2cea50fbe695
Tim Barton <tim@vouchercloud.com>
2017-09-08 09:36:11 +0100

    Removed package script

--- build.bat
+++ build.bat
@@ -13,17 +13,14 @@ REM Package restore
 call %NuGet% restore CircuitBreaker.Tests\packages.config -OutputDirectory %cd%\packages -NonInteractive
 
 REM Build
-call "%msbuild%" IDL.Net.CircuitBreaker.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false %version%
+call "%msbuild%" IDL.Net.CircuitBreaker.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 if not "%errorlevel%"=="0" goto failure
 
 REM Unit tests
 "%GallioEcho%" CircuitBreaker.Tests\bin\%config%\CircuitBreaker.Tests.dll
 if not "%errorlevel%"=="0" goto failure
 
-REM Package
-mkdir Build
-call %nuget% pack "CircuitBreaker\CircuitBreaker.nuspec" -symbols -o Build -p Configuration=%config% %version%
-if not "%errorlevel%"=="0" goto failure
+
 
 :success
 exit 0
